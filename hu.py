806f0b8aad69ccc769bb323965d2fe7b8537e883
xzsss <1151856155@qq.com>
2021-12-09 21:11:45 +0800

    Create hu.py
    add file

---
+++ hu.py
@@ -0,0 +1,40 @@
+import http.client
+import requests
+import threading
+import time
+def printHello(): 
+    global isthere 
+    nowhour =str(time.strftime("%H", time.localtime()))
+    if nowhour in ['8','9','10','17','18','19','20','21']:
+        url = "https://bmeparkapi.capitaland.com.cn/api/park/guest_get_parked_info?mall_id=96&car_no=%E8%8B%8FE88F69&is_test=0&mallId=96&portalId=82"
+        payload={}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        datatext = response.text
+        if '操作成功' not in datatext:
+            print(datatext)
+            exit()
+        if '查询到当前车辆的入场信息' in datatext:
+            newstatus=False
+            message ="%E8%83%A1%E8%80%81%E5%B8%88%E8%B5%B0%E4%BA%86"
+        else:
+            newstatus= True
+            message ="%E8%83%A1%E8%80%81%E5%B8%88%E4%BB%96%E6%9D%A5%E4%BA%86"
+        if newstatus!=isthere:
+            conn = http.client.HTTPSConnection("sctapi.ftqq.com")
+            payload = ''
+            headers = {}
+            conn.request("GET", "/SCT102166TKyxrHIebvjsU5ExLn5zljMjT.send?title="+message+"&desp="+message, payload, headers)
+            res = conn.getresponse()
+            data = res.read()
+            print(data.decode("utf-8"))
+            isthere=newstatus
+            if 'SUCCESS' not in data.decode("utf-8"):
+                print(data.decode("utf-8"))
+                exit()
+    timer = threading.Timer(600,printHello)
+    timer.start()
+if __name__ == "__main__":  
+    isthere =False
+    timer = threading.Timer(1,printHello)
+    timer.start()           
